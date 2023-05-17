687817443dd772cb904e6c360266c6100c17d1b9
MonoChyvo <mescal.13@proton.me>
2023-05-17 01:13:42 -0600

    update

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/api/notes', (request, response) => {
   response.sendStatus(201).json(newNote)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
